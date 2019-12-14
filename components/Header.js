27b6fde48a8736269519ecfefec7d166d95be800
ParkMinKwan <jik2586@gmail.com>
2019-12-14 22:20:14 +0900

    global css 구성

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,42 @@
 import styled from 'styled-components';
+import Link from 'next/link';
 
 const MainFrame = styled.div`
   width: 100%;
   height: 100px;
+  position: relative;
   border: 1px solid black;
+  font-family: 'Roboto';
+  font-weight: 500;
+
+  & > h1 {
+    display: inline-block;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+`
+
+const Logo = styled.a`
+  width: 59px;
+  height: 29px;
+  display: block;
+  background: url(${props => props.url}) 0 0 no-repeat;
+  background-size: 100% auto;
+  overflow: hidden;
+  text-indent: -9999px;
+  line-height: 0;
 `
 
 const Header = () => {
   return (
     <MainFrame>
-      Header 입니다.
+    <h1 className="Logo">
+      <Link href="/">
+        <Logo url="/images/logo.png">Media4th &amp; Company Logo </Logo>
+      </Link>
+    </h1>
     </MainFrame>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import Header from '../components/Header';
 const Home = () => (
   <div>
     <Header />
-    <img src="/images/logo.png"  alt="logo" />
   </div>
 )
 
